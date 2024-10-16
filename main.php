fc984e89e6e01b9f1dc15f0940b6aea06008fc2b
NGOCHAI <phamngochai0209@gmail.com>
2024-10-16 11:16:35 +0900

    hai

--- main.php
+++ main.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>WRB - Home</title>
     <link rel="stylesheet" href="./styles/index.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
 </head>
 <body>
     <!-- Navbar -->
@@ -13,6 +14,20 @@
         <input type="text" class="search-bar" placeholder="Search...">
         <h1 class="logo">WRB</h1>
     </div>
+    <nav class="nav-menu">
+        <ul>
+          <li><a href="main.php">ホームページ</a></li>
+          <li><a href="#">商品</a></li>
+          <li><a href="#">お店について</a></li>
+          <li><a href="#">登録</a></li>
+          <li><a href="#">ログイン</a></li>
+          <li class="support-title">サポート</li>
+          <li class="support"><i class="fa fa-phone"></i><a class="support" href="tel">+81000000000000</a></li>
+          <li class="support"><i class="fa fa-envelope"></i><a class="support" href="mail:">srb@ecc.ac.jp</a></li>
+          <li class="support"><i class="fa fa-map-marker"></i><a target="blank" class="support" href=""></a></li>
+         </ul>
+       </nav>
+       <div class="overlay"></div>
 
     <!-- Category Section -->
     <section class="category">
@@ -71,6 +86,7 @@
                 <img src="./images/twitter.png" alt="Coat" />
             </div>
         </div>
+        <script src="./scripts/menu.js"></script>
     </section>
 </body>
 </html>
