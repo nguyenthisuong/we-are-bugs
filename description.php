a20851b231843b80d8cd73012d65bafa08ae7806
Tyla21 <150421166+Tyla21@users.noreply.github.com>
2024-10-18 13:57:09 +0900

    description

---
+++ description.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>WRB - Description</title>
+    <link rel="stylesheet" href="./styles/description.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
+</head>
+<body>
+    <header></header>
+    <main></main>
+    
+    <div>
+        <!-- Top bar -->
+        <div class="top-bar">
+            <a href="#" class="back-button"><img src="./images/backicon.png" alt="Back"></a> 
+            <a href="#" class="heart-icon"><img src="./images/white-heart.png" alt="Heart"></a> 
+        </div>
+
+        <!-- Image Section -->
+        <div class="image-section">
+            <img src="facebook.png" alt="">
+        </div>
+
+        <!-- Description Section -->
+        <div class="description">
+            <h2>Long Soft Waistcoat</h2>
+            <p class="price">6,290 ¥</p>
+            <p>Round neck waistcoat. Front pockets. Front fastening with buttons.</p>
+        </div>
+        
+        <!-- Add to Cart Button -->
+        <div class="add-to-cart">
+            <button>Add to Cart</button>
+        </div>
+    </div> 
+</body>
+</html>
