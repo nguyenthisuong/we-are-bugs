c192ac0ad24746b73192e2ba64be26732e375009
NGOCHAI <phamngochai0209@gmail.com>
2024-10-18 13:46:17 +0900

    oke

--- main.php
+++ main.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>WRB - Home</title>
     <link rel="stylesheet" href="./styles/index.css">
+    <link rel="stylesheet" href="./styles/All.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
     
 </head>
