0fe238806cdbea707af40196f96ab61d91bf487d
NGOCHAI <phamngochai0209@gmail.com>
2024-10-18 13:41:22 +0900

    a

---
+++ coppy.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>WRB - Home</title>
+    <link rel="stylesheet" href="./styles/All.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
+    
+</head>
+<body>
+    <header>
+        <!-- Navbar -->
+    <div class="navbar">
+        <div class="menu-icon">☰</div>
+        <input type="text" class="search-bar" placeholder="Search...">
+        <h1 class="logo">WRB</h1>
+    </div>
+    <nav class="nav-menu">
+        <ul>
+          <li><a href="main.php">ホームページ</a></li>
+          <li><a href="#">商品</a></li>
+          <li><a href="#">お店について</a></li>
+          <li><a href="#">会員登録</a></li>
+          <li><a href="#">ログイン</a></li>
+          <li class="support-title">サポート</li>
+          <li class="support"><i class="fa fa-phone"></i><a class="support" href="tel">+81000000000000</a></li>
+          <li class="support"><i class="fa fa-envelope"></i><a class="support" href="mail:">srb@ecc.ac.jp</a></li>
+          <li class="support"><i class="fa fa-map-marker"></i><a target="blank" class="support" href=""></a></li>
+         </ul>
+    </nav>
+       <div class="overlay"></div>
+    </header>
+
+       <main>
+       <h2>MAIN</h2>
+       <h2>MAIN</h2>
+       <h2>MAIN</h2>
+       <h2>MAIN</h2>
+       <h2>MAIN</h2>
+       <h2>MAIN</h2>
+       <h2>MAIN</h2>
+       </main>
+
+</body>
+<footer>
+<script src="./scripts/menu.js"></script>
+</footer>
+</html>
